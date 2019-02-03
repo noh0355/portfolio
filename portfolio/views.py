79bfdf63c47ac96e47ceaa09ad76444dce6830c6
noh0355 <noh0355@likelion.org>
2019-02-03 22:09:57 +0900

    portfolio

---
+++ portfolio/views.py
@@ -0,0 +1,11 @@
+
+from django.shortcuts import render
+
+from .models import Portfolio
+# Create your views here.
+
+
+def portfolio(request):
+    portfolios = Portfolio.objects
+    return render(request, 'portfolio.html', {'portfolios': portfolios})
+# Create your views here.
